b3630a644bf1c3e3662e6428e16c951b25b08c80
Subhransu Sekhar Sahoo <subhransu.sahoo@enquero.com>
2020-03-10 13:28:42 +0530

    react app.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import App from '/Users/SubhransuSekharSahoo/playground/src/App'
+  
+ReactDOM.render(<App></App>,document.getElementById('root'));
